4c2f9008fb136324124a60b645f5bd9e7c2e5941
AM1209 <dimos@uth.gr>
2018-06-21 23:47:50 +0300

    added export

--- hw1.c
+++ hw1.c
@@ -14,12 +14,13 @@
 
 void input(int fd, char *path);
 void find(int fd, char *name);
-int exists (int fd, char *name);
+void export(int fd, char *name, char *dest);
+int exists (int fd, char *name);  //if found returns start of object, if not found returns -1
 
 int main(int argc,char *argv[]){
 
     int fd;
-	char action[MAX_SIZE];
+	char action[MAX_SIZE], *dest, name[MAX_SIZE];
 
     if((fd = open(argv[1],O_RDWR|O_CREAT,0666))==-1){
         printf("open %d\n",errno);
@@ -37,10 +38,12 @@ int main(int argc,char *argv[]){
                 break;
             case 'f':
 				find(fd,&action[2]);
-                //find(name);
                 break;
             case 'e':
-                //export(name,dest);
+				dest=strchr(&action[2],' ')+sizeof(char); //dest from empty+1 to end
+				strncpy(name,&action[2],strlen(&action[2])-strlen(dest));
+				name[strlen(&action[2])-strlen(dest)-1]='\0';
+				export(fd, name, dest);
                 break;
             case 'd':
                 //delete(name);
@@ -86,8 +89,8 @@ void input(int fd, char *path){
 	}
 
 	//check if name already exists in db
-	
-	if(exists(fd,name)){
+	if(exists(fd,name)!=-1){
+		printf("Error, item already exists in db\n");
 		return;
 	}
 	
@@ -131,12 +134,10 @@ int exists (int fd, char *name){
 			read(fd,buf,strlen(name)); 
 			buf[strlen(name)]='\0';
 			if(strcmp(name,buf)==0){  //if same name
-				printf("Error item already exists\n");
-				return 1;
+				return lseek(fd,(off_t)-strlen(name)-MAX_INT-2,SEEK_CUR);;
 			}
 			else{
 				lseek(fd,(off_t)1,SEEK_CUR);
-				
 			}
 		}
 		else{
@@ -146,7 +147,7 @@ int exists (int fd, char *name){
 		buf[MAX_INT]='\0';
 		lseek(fd,(off_t) strtol(buf,&ptr,10)+2,SEEK_CUR);
 	}
-	return 0;
+	return -1;
 }
 
 void find(int fd,char *name){
@@ -166,4 +167,39 @@ void find(int fd,char *name){
 		buf[MAX_INT]='\0';
 		lseek(fd,(off_t) strtol(buf,&ptr,10)+2,SEEK_CUR);
 	}
+}
+
+void export(int fd, char *name, char *dest){
+
+	int pos, fd2, end;
+	char buf[MAX_READ], *ptr;
+
+	if((fd2 = open(dest,O_RDWR|O_CREAT|O_EXCL,0666))==-1){
+        printf("Export:File already exists %d\n",errno);
+		return;
+    }
+	
+	
+	if ((pos=exists(fd,name))!=-1){
+		//read from input file
+		lseek(fd,(off_t) pos+3+strlen(name)+MAX_INT, SEEK_SET);  //find end
+		read(fd,buf,MAX_INT); //skip data
+		buf[MAX_INT]='\0';
+		end=lseek(fd,(off_t) strtol(buf,&ptr,10)+1,SEEK_CUR);
+
+
+		//write to dest
+		lseek(fd,(off_t) pos, SEEK_SET);
+		for(int i=0;i<(end-pos)/MAX_READ;i++){  //get object
+			read(fd,buf,MAX_READ);
+			write(fd2,buf,MAX_READ);
+		}
+		read(fd,buf,(end-pos)%MAX_READ);
+		write(fd2,buf,(end-pos)%MAX_READ);
+	}
+	else{ 
+		return;
+	}
+
+
 }
